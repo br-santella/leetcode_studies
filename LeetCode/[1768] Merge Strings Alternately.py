eaa6e7349122a4d7f721899f9d284daac9df059e
Brunna Santella <brunna.santella@gmail.com>
2026-05-25 10:09:50 -0300

    Update [1768] Merge Strings Alternately.py

--- LeetCode/[1768] Merge Strings Alternately.py
+++ LeetCode/[1768] Merge Strings Alternately.py
@@ -1,4 +1,5 @@
-# Merge 2 string with max 100 letters
+# Merge 2 string with max 100 letters [Easy][Acceptance: 82,1%]
+# https://leetcode.com/problems/merge-strings-alternately/description
 
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
@@ -17,10 +18,3 @@ class Solution:
 
 sol = Solution() 
 final_output = sol.mergeAlternately("abc", "pqr")
-
-"""QUESTIONS
-1. For what is used the self parameter in the function? Is necessary?
-    Used for OOP. Self represents the specific object builted. It allows the functions inside the class to access or change that specific object's data.
-2. Why to use a class Solution?
-    To test your code, LeetCode has to run your function against hundreds of different test cases. By using a class, the platform can easily spin up a fresh, isolated instance of your code for every single test case, ensuring that leftover data from Test Case #1 doesn't accidentally mess up Test Case #2.
-"""
