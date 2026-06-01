d1d5d7bb2bcdef0189403baec05851e218e4fbb7
Brunna Santella <brunna.santella@gmail.com>
2026-05-31 23:10:12 -0300

    Add solution for moving zeroes in an array
    Implement solution to move all zeroes in the list to the end while maintaining the order of non-zero elements.

---
+++ LeetCode/Easy/283-move-zeroes.py
@@ -0,0 +1,11 @@
+# Move Zeroes [Easy][Acceptance: 63,9%]
+# https://leetcode.com/problems/move-zeroes/description
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        left = 0  
+        
+        for right in range(len(nums)):
+            if nums[right] != 0:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
